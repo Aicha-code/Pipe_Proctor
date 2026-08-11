4dafc037fa52bef28378c963a0470538b05b3aef
Abari Aichetou <ug2321291@ines.ac.rw>
2026-08-12 00:05:03 +0200

    initialize pretrained model for experiment

--- experiment.py
+++ experiment.py
@@ -1,10 +1,25 @@
 from use_croma import PretrainedCROMA
+import torch
 
 """
 this is an experiment at using the pretrained CROMA model for SAR images.
 I am adding comments at each step for what will need to be updated/modified when we get our training data.
-Depending on the dataset's size, I might have to finetune the model on our dataset, but I will first try to use it as is and see how it performs.
+I might have to finetune CROMA on our dataset, but I will use it as is to understand it better.
 """
-# To experiment with CROMA, O downloaded the model weights from https://huggingface.co/antofuller/CROMA/tree/main
+# To experiment with CROMA, I downloaded the model weights from https://huggingface.co/antofuller/CROMA/tree/main
 model = PretrainedCROMA(pretrained_path='CROMA_base.pt', size='base', modality='SAR', image_resolution=120)
 # The model will not work if the input images are of a different size than the one specified here. 
+
+# sentinel_SAR_image = torch.rand(1, 1, 120, 120)
+# The model expects the input to be a tensor of shape (batch_size, channels, height, width). with channels== 1 for SAR images.
+print("model loaded ✅!")
+
+#sample dummy input to test the model
+N = 4
+
+sentinel_1 = torch.randn(N, 2, 120, 120)
+print(f"input transformsation:\n shape: {sentinel_1.shape}\n type: {type(sentinel_1)}\n dtype: {sentinel_1.dtype}")
+
+with torch.no_grad():
+    outputs = model(sentinel_1)
+# print model output information
